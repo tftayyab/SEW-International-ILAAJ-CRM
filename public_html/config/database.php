403de27893686038adf95108c17809e55c963442
hanzalamaw <hanzalamawahab@gmail.com>
2026-08-14 17:37:04 +0500

    Folder Structure Change

--- public_html/config/database.php
+++ public_html/config/database.php
@@ -6,8 +6,8 @@
 return [
     'host' => getenv('DB_HOST') ?: '127.0.0.1',
     'port' => getenv('DB_PORT') ?: '3306',
-    'dbname' => getenv('DB_NAME') ?: 'ilaaj_crm',
-    'username' => getenv('DB_USER') ?: 'root',
-    'password' => getenv('DB_PASS') ?: '',
+    'dbname' => getenv('DB_NAME') ?: 'webhngff_ilaaj_crm',
+    'username' => getenv('DB_USER') ?: 'webhngff_hanzalamaw',
+    'password' => getenv('DB_PASS') ?: 'Hani2006!',
     'charset' => 'utf8mb4',
 ];
